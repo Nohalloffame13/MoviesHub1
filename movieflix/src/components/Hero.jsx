7150b71eb9d522371691e0fff1d0a432de477df0
Nohalloffame13 <168319922+Nohalloffame13@users.noreply.github.com>
2026-07-06 11:19:52 +0530

    Create Hero.jsx

---
+++ movieflix/src/components/Hero.jsx
@@ -0,0 +1,53 @@
+import "./Hero.css";
+
+export default function Hero() {
+
+return(
+
+<section className="hero">
+
+<div className="overlay">
+
+<div className="content">
+
+<h1>Watch Movies Anytime</h1>
+
+<p>
+
+Stream your favorite movies in HD quality.
+
+New movies added every week.
+
+</p>
+
+<div className="buttons">
+
+<button className="watch">
+▶ Watch Now
+</button>
+
+<button className="info">
+More Info
+</button>
+
+</div>
+
+<div className="details">
+
+<span>⭐ 9.5/10</span>
+
+<span>HD</span>
+
+<span>2026</span>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+)
+
+}
