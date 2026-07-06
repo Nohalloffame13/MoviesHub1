162f8e3caa79b5f3294dc94d599247f8253cbf90
Nohalloffame13 <168319922+Nohalloffame13@users.noreply.github.com>
2026-07-06 11:21:23 +0530

    Create movies.js

---
+++ movieflix/src/data/movies.js
@@ -0,0 +1,32 @@
+const movies = [
+
+{
+id:1,
+title:"Avengers Endgame",
+category:"Action",
+rating:"9.3",
+poster:"https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
+driveId:"YOUR_GOOGLE_DRIVE_ID"
+},
+
+{
+id:2,
+title:"Avatar",
+category:"Sci-Fi",
+rating:"9.1",
+poster:"https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+driveId:"YOUR_GOOGLE_DRIVE_ID"
+},
+
+{
+id:3,
+title:"John Wick 4",
+category:"Action",
+rating:"9.0",
+poster:"https://image.tmdb.org/t/p/w500/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+driveId:"YOUR_GOOGLE_DRIVE_ID"
+},
+
+];
+
+export default movies;
